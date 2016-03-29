4b39b63e281d1c4989ec50e928de2ac5e163f8f5
AdaHsu <532864382@qq.com>
2016-03-28 19:45:36 -0700

    Initial Commit

---
+++ KVOPrac/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KVOPrac
+//
+//  Created by Mac on 16/3/28.
+//  Copyright © 2016年 Ada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
